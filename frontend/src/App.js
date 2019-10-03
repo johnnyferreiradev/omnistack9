230851510803434ecf531c40fc6e512bab78d0d4
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-03 16:42:21 -0300

    Inicio do frond e adição do cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
+  const [email, setEmail] = useState('');
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const response = await api.post('/sessions', { email });
+
+    console.log(response);
+  }
+
+
   return (
-    <h1>Hello World!</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnc"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
