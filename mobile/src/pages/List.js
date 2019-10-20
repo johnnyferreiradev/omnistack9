4e6e4e3b255fdffb1c6baf1778f51b0913ec3876
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-20 13:00:41 -0300

    Conclusão da criação base do app mobile

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { view } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Image, StyleSheet, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storageTechs => {
+            const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        });
+    }, []);
+
     return (
-        <View>
-            
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList tech={tech} key={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 30
+    }
+});
